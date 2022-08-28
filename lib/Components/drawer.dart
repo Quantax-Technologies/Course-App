7709346a915e1001ed4cef32fb90e0f28b904fbe
unknown <shahzeb.naqvi12@gmail.com>
2022-08-29 01:05:02 +0500

    course add

---
+++ lib/Components/drawer.dart
@@ -0,0 +1,61 @@
+import 'package:courseapp/Screen/Auth/logout_screen.dart';
+import 'package:courseapp/Screen/add_course.dart';
+import 'package:courseapp/Theme/color.dart';
+import 'package:courseapp/Theme/resources.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Drawer Drawermain(context) {
+  return Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        DrawerHeader(
+          child: Text('Drawer Header'),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+        ),
+        ListTile(
+          title: Text('Add Course'),
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => AddCourse()));
+          },
+        ),
+        ListTile(
+          title: Text('Profile'),
+          onTap: () {
+            // Update the state of the app.
+            // ...
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => Logout()));
+          },
+        ),
+      ],
+    ),
+  );
+}
+
+Drawer Drawermain2(context) {
+  return Drawer(
+    child: ListView(
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        DrawerHeader(
+          child: Text('Drawer Header'),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+          ),
+        ),
+        ListTile(
+          title: Text('About'),
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (context) => AddCourse()));
+          },
+        ),
+      ],
+    ),
+  );
+}
